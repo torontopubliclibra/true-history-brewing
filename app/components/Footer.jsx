94981b61873e8825b602424f4b0f46a69c70bb25
torontopubliclibra <dana.r.teagle@gmail.com>
2023-12-14 21:59:28 -0500

    feat: updated footer return

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,14 +1,28 @@
 import {Link} from '@remix-run/react';
+import {useLocation} from 'react-router-dom';
 
 export function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footer-buttons">
-        <Link to="https://instagram.com/truehistorybrewing">Instagram</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
-      <p>Copyright True History Brewing, 2024</p>
-      <p>Website designed by <a href="https://danateagle.com">Dana Teagle</a></p>
-    </footer>
-  );
+  const location = useLocation();
+
+  if (location.pathname === '/') {
+    
+    return (
+      <footer className="footer">
+      </footer>
+    )
+
+  } else {
+
+    return (
+      <footer className="footer">
+        <div className="footer-buttons">
+          <Link to="https://instagram.com/truehistorybrewing">Instagram</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <p>Copyright True History Brewing, 2024</p>
+        <p>Website designed by <a href="https://danateagle.com">Dana Teagle</a></p>
+      </footer>
+    );
+
+  };
 };
