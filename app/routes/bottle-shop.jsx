b0e582d6e1ecb36357c42001910bf806ed70dbfb
torontopubliclibra <dana.r.teagle@gmail.com>
2023-12-14 22:16:13 -0500

    feat: initialized empty bottle shop and contact routes

---
+++ app/routes/bottle-shop.jsx
@@ -0,0 +1,19 @@
+/**
+ * @type {MetaFunction}
+ */
+export const meta = () => {
+  return [
+    {title: 'True History Brewing | Toronto, ON'},
+    {description: 'Brewing Low & Slow'},
+  ];
+};
+
+export default function Homepage() {
+  return (
+    <>
+        <section className="bottle-shop-heading">
+          <h2>Bottle Shop</h2>
+        </section>
+    </>
+  );
+};
---
+++ app/routes/contact.jsx
@@ -0,0 +1,19 @@
+/**
+ * @type {MetaFunction}
+ */
+export const meta = () => {
+  return [
+    {title: 'True History Brewing | Toronto, ON'},
+    {description: 'Brewing Low & Slow'},
+  ];
+};
+
+export default function Homepage() {
+  return (
+    <>
+        <section className="contact-heading">
+          <h2>Contact</h2>
+        </section>
+    </>
+  );
+};
