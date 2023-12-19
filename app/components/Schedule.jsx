0aff1ad0ce74abb197c7285172b54ee6627143e2
torontopubliclibra <dana.r.teagle@gmail.com>
2023-12-18 22:52:21 -0500

    feat: updates to schedule component

--- app/components/Schedule.jsx
+++ app/components/Schedule.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react';
+import diamond from './../../public/assets/icons/diamond.svg';
 
 export function Schedule() {
 
@@ -35,38 +36,45 @@ export function Schedule() {
   
     let taproomHours = {
       Mon: "Closed",
-      Tues: "4:00pm-11:00pm",
-      Weds: "4:00pm-11:00pm",
-      Thurs: "4:00pm-11:00pm",
-      Fri: "12:00pm-12:00am",
-      Sat: "12:00pm-12:00am",
-      Sun: "12:00pm-10:00pm"
+      Tues: "4:00pm–11:00pm",
+      Weds: "4:00pm–11:00pm",
+      Thurs: "4:00pm–11:00pm",
+      Fri: "12:00pm–12:00am",
+      Sat: "12:00pm–12:00am",
+      Sun: "12:00pm–10:00pm"
     };
   
     if (schedule.publishedAt) {
   
       taproomHours = {
         Mon: "Closed",
-        Tues: convertTimeToAmPm(schedule.tues_start) + `-` + convertTimeToAmPm(schedule.tues_end),
-        Weds: convertTimeToAmPm(schedule.weds_start) + `-` + convertTimeToAmPm(schedule.weds_end),
-        Thurs: convertTimeToAmPm(schedule.thurs_start) + `-` + convertTimeToAmPm(schedule.thurs_end),
-        Fri: convertTimeToAmPm(schedule.fri_start) + `-` + convertTimeToAmPm(schedule.fri_end),
-        Sat: convertTimeToAmPm(schedule.sat_start) + `-` + convertTimeToAmPm(schedule.sat_end),
-        Sun: convertTimeToAmPm(schedule.sun_start) + `-` + convertTimeToAmPm(schedule.sun_end)
+        Tues: convertTimeToAmPm(schedule.tues_start) + `–` + convertTimeToAmPm(schedule.tues_end),
+        Weds: convertTimeToAmPm(schedule.weds_start) + `–` + convertTimeToAmPm(schedule.weds_end),
+        Thurs: convertTimeToAmPm(schedule.thurs_start) + `–` + convertTimeToAmPm(schedule.thurs_end),
+        Fri: convertTimeToAmPm(schedule.fri_start) + `–` + convertTimeToAmPm(schedule.fri_end),
+        Sat: convertTimeToAmPm(schedule.sat_start) + `–` + convertTimeToAmPm(schedule.sat_end),
+        Sun: convertTimeToAmPm(schedule.sun_start) + `–` + convertTimeToAmPm(schedule.sun_end)
       };
     }
   
     let formattedHours = [];
   
     for (let [key, value] of Object.entries(taproomHours)) {
-      formattedHours.push(<li>{key}: {value}</li>);
+      formattedHours.push(<li>{key}: <span className='time'>{value}</span></li>);
     };
 
     return <div className="hours">
-        <p>Hours this Week:</p>
-        <ul>
-        {formattedHours}
-        </ul>
-    </div>;
+              <h4>Hours this Week</h4>
+              <div className="diamond-border">
+                <img src={diamond} className="button-icon" />
+                <img src={diamond} className="button-icon" />
+                <img src={diamond} className="button-icon" />
+                <img src={diamond} className="button-icon" />
+                <img src={diamond} className="button-icon" />
+              </div>
+              <ul className='schedule'>
+              {formattedHours}
+              </ul>
+            </div>;
 
 }
