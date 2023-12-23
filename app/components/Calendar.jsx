e6310520e48a2b939ca464765a053d06ac150a2a
torontopubliclibra <dana.r.teagle@gmail.com>
2023-12-23 09:47:20 -0500

    feat: adding list view to calendar component

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -1,13 +1,14 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import listPlugin from '@fullcalendar/list'
 
 export function Calendar() {
 
 return  <div className="calendar">
             <h4>Calendar</h4>
             <FullCalendar
-                plugins={[ dayGridPlugin ]}
-                initialView="dayGridMonth"
+                plugins={[ dayGridPlugin, listPlugin ]}
+                initialView="listMonth"
                 events={[
                     { title: 'Trivia night', date: '2024-01-09' }
                     ]}
