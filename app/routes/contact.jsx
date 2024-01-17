0a0a6c3f490439735ac13c15cd3aec2f52b8f7cd
torontopubliclibra <dana.r.teagle@gmail.com>
2024-01-16 20:51:40 -0500

    feat: building contact page

--- app/routes/contact.jsx
+++ app/routes/contact.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /**
  * @type {MetaFunction}
  */
@@ -9,12 +11,44 @@ export const meta = () => {
 };
 
 export default function Homepage() {
+
+  let [ contactForm, setContactForm ] = useState("general");
+
+  let handleContactChange = (form) => {
+    setContactForm(form);
+  }
+
+  let buttonClass = (button) => {
+    if (button == contactForm) {
+      return 'button button-primary selected';
+    } else {
+      return 'button button-primary';
+    }
+  }
+
   return (
     <>
         <section className="heading contact-heading">
           <h2>Contact</h2>
         </section>
         <section className="contact-content">
+          <ul className='contact-nav'>
+            <li>
+              <button onClick={() => handleContactChange("general")} className={buttonClass("general")}>
+                General
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleContactChange("licensee")} className={buttonClass("licensee")}>
+                Licensees
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleContactChange("events")} className={buttonClass("events")}>
+                Event booking
+              </button>
+            </li>
+          </ul>
         </section>
     </>
   );
