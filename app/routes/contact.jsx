f800cb3f363d127f8f2ee2d9559dee13dc2ea1c3
torontopubliclibra <dana.r.teagle@gmail.com>
2024-01-17 00:33:26 -0500

    feat: building out contact functionality

--- app/routes/contact.jsx
+++ app/routes/contact.jsx
@@ -13,6 +13,12 @@ export const meta = () => {
 export default function Homepage() {
 
   let [ contactForm, setContactForm ] = useState("general");
+  let [ submitStatus, setSubmitStatus ] = useState('Submit');
+  let [ messageDetails, setMessageDetails ] = useState({ 
+    name: "",
+    email: "",
+    message: "",
+  });
 
   let handleContactChange = (form) => {
     setContactForm(form);
@@ -26,6 +32,109 @@ export default function Homepage() {
     }
   }
 
+  let handleSubmit = async (event) => {
+
+    event.preventDefault();
+    setSubmitStatus("Sending...");
+
+    const { name, email, message } = event.target.elements;
+    setMessageDetails({
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    });
+    // let response = await fetch("http://localhost:5000/contact", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   },
+    //   body: JSON.stringify(details),
+    // });
+    setSubmitStatus("Submit");
+    // let result = await response.json();
+    // alert(result.status);
+  };
+
+  let ContactForm = () => {
+    if (contactForm == "general") {
+      return (
+        <form onSubmit={handleSubmit} className="contact-form">
+          <div className="form-field">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" required />
+          </div>
+          <button className="button" type="submit">{submitStatus}</button>
+        </form>
+      );
+    } else if (contactForm == "licensee") {
+      return (
+        <form onSubmit={handleSubmit} className="contact-form">
+          <div className="form-field">
+            <label htmlFor="business-name">Business name:</label>
+            <input type="text" id="business-name" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="business-address">Business address:</label>
+            <input type="text" id="business-address" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" required />
+          </div>
+          <button className="button" type="submit">{submitStatus}</button>
+        </form>
+      );
+    } else if (contactForm == "events") {
+      return (
+        <form onSubmit={handleSubmit} className="contact-form">
+          <div className="form-field">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="double-field">
+            <div className="small-field">
+              <label htmlFor="guests"># of guests:</label>
+              <input type="number" id="guests" required />
+            </div>
+            <div className="small-field">
+              <label htmlFor="dining">Dining? Y/N</label>
+              <input type="radio" id="dining" required />
+            </div>
+          </div>
+          <div className="form-field">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" required />
+          </div>
+          <div className="form-field">
+            <label htmlFor="special">Special requests:</label>
+            <textarea id="special" required />
+          </div>
+          <button className="button" type="submit">{submitStatus}</button>
+        </form>
+      );
+    }
+  };
+
   return (
     <>
         <section className="heading contact-heading">
@@ -35,12 +144,12 @@ export default function Homepage() {
           <ul className='contact-nav'>
             <li>
               <button onClick={() => handleContactChange("general")} className={buttonClass("general")}>
-                General
+                General inquiries
               </button>
             </li>
             <li>
               <button onClick={() => handleContactChange("licensee")} className={buttonClass("licensee")}>
-                Licensees
+                Licensees & retailers
               </button>
             </li>
             <li>
@@ -49,6 +158,7 @@ export default function Homepage() {
               </button>
             </li>
           </ul>
+          <ContactForm />
         </section>
     </>
   );
