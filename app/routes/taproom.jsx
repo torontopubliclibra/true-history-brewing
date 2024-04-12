5e472d9f35ba2cba5a6d4b05d3bc366889a2846a
torontopubliclibra <dana.r.teagle@gmail.com>
2024-04-12 10:37:12 -0400

    feat: added a text box for food menu description

--- app/routes/taproom.jsx
+++ app/routes/taproom.jsx
@@ -268,6 +268,12 @@ export default function Homepage() {
                         </li>
                       </ul>
                       <ul className='menu'>
+                        { selectedMenu === 'food'
+                          ? <div className="food-menu-description">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                            </div>
+                          : null
+                        }
                         {formattedMenus[selectedMenu]}
                       </ul>
                     </>
